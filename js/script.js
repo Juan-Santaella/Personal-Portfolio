15f28155170232b1879cffe7a19551067d5ccdf9
Juan Santaella <jsantaellamandillo@gmail.com>
2021-12-04 20:10:49 +0000

    home

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 let menu = document.querySelector('#menu-bars');
 let header = document.querySelector('header');
 
+/*Cambio de la imagen de la hamburguesa, cuando se hace click sobre ella*/
 menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     header.classList.toggle('active');
 }
 
+/*Ocultavmos el nav y el header en el Responsive Design*/
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     header.classList.remove('active');
 }
 
+
+/*Cursores de ratón*/
 let cursor1 = document.querySelector('.cursor-1');
 let cursor2 = document.querySelector('.cursor-2');
 
@@ -20,3 +24,18 @@ window.onmousemove = (e) =>{
     cursor2.style.top = e.pageY + 'px';
     cursor2.style.left = e.pageX + 'px';
 }
+
+/*Cambio de la forma del cursor al pasar sobre un item del nav*/
+document.querySelectorAll('a').forEach(links =>{
+
+    links.onmouseenter = () =>{
+        cursor1.classList.add('active');
+        cursor2.classList.add('active');
+    }
+
+    links.onmouseleave = () =>{
+        cursor1.classList.remove('active');
+        cursor2.classList.remove('active');
+    }
+
+});
