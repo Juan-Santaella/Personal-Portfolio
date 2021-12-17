d643427df450ddd8700f8429ac40fb7e5850011b
Juan Santaella <jsantaellamandillo@gmail.com>
2021-12-17 00:28:53 +0000

    Portfolio

---
+++ assets/mail_contacto.php
@@ -0,0 +1,20 @@
+<?php
+    $nombre=$_POST["nombre"];
+    $email=$_POST["email"];
+    $telefono=$_POST["telefono"];
+    $asunto=$_POST["asunto"];
+    $mensaje=$_POST["mensaje"];
+
+    $para="info@j-santaella.com";
+
+    $cuerpo_mensaje="
+        Nombre del remitente: ".$nombre."
+        Correo: ".$email."
+        Teléfono: ".$telefono."
+        Mensaje: ".$mensaje."
+    ";
+
+    mail($para,$asunto,utf8_decode($mensaje));
+
+    header("location: ../index.html");      //Nos redirige a la página principal
+?>
